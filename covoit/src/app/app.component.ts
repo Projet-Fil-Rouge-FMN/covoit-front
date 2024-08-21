5761719d09ebfed72f58c723a3816608e920ab15
Noémie Letenneur <n.letenneur@gmail.com>
2024-08-21 12:49:44 +0200

    Create model, service and component-item/component-list for Route #6

--- covoit/src/app/app.component.ts
+++ covoit/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { UserListComponent } from "./components/user/user-list/user-list.component";
+import { UserListComponent } from './components/user/user-list/user-list.component';
+import { RouteListComponent } from './components/route/route-list/route-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserListComponent],
+  imports: [RouterOutlet, UserListComponent, RouteListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'covoit';
---
+++ covoit/src/app/models/route.model.ts
@@ -0,0 +1,9 @@
+import Address from Address.model.ts
+
+export interface Route {
+  id: number;
+  duration: number;
+  kmTotal: number;
+  startAddress: Address[];
+  endAddress: Address[];
+}
