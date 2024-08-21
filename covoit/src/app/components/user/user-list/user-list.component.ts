6d34bfe0f973520692bd70a65413c8a9fa33f07b
MiL-Dev <mil.devacc@gmail.com>
2024-08-21 11:57:42 +0200

    resolve HttpClient

---
+++ covoit/src/app/components/user/user-list/user-list.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../../../models/user.model';
+import { UserService } from '../../../services/user.service';
+import { AsyncPipe } from '@angular/common';
+import { UserItemComponent } from '../user-item/user-item.component';
+
+@Component({
+  selector: 'app-user-list',
+  standalone: true,
+  imports: [AsyncPipe, UserItemComponent],
+  templateUrl: './user-list.component.html',
+  styleUrl: './user-list.component.css'
+})
+export class UserListComponent {
+  users$: Observable<User[]>;
+
+  constructor(private userService: UserService){
+    this.users$ = this.userService.getUsers();
+  }
+
+}
---
+++ covoit/src/app/services/user.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+private apiUrl = 'http://localhost:8081/user/';
+  constructor(private http: HttpClient) {}
+
+  getUsers(): Observable<User[]>{
+    return this.http.get<User[]>(this.apiUrl);
+  }
+}
