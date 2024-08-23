79117f277c44f20c10c49483a9a2bba69c407832
frederic smée <ssdfred@yahoo.fr>
2024-08-23 15:51:52 +0200

    resolution de probleme

--- covoit/src/app/components/auth/login/login.component.ts
+++ covoit/src/app/components/auth/login/login.component.ts
@@ -1,52 +1,47 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { inject, Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
-
-import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     ReactiveFormsModule,
-    RouterModule,
 
   ],
-  providers: [AuthService],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  form: FormGroup;
 
-  constructor(
-    private fb: FormBuilder,
-    private authService: AuthService,
-    private router: Router
-  ) {
-    this.form = this.fb.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required],
-    });
-  }
+  private authService = inject(AuthService);
+  private router = inject(Router);
 
-  login(): void {
-    if (this.form.valid) {
-      const { username, password } = this.form.value;
-      this.authService.login(username, password).subscribe({
-        next: (response) => {
-          // Stocker les informations utilisateur dans le localStorage 
-          localStorage.setItem('user', JSON.stringify(response));
-          // Redirection vers la page d'accueil après une connexion réussie
-          this.router.navigate(['/']);
-        },
-        error: (error) => {
-          // Gestion des erreurs de connexion
-          console.error('Erreur de connexion', error);
-        }
-      });
+  protected loginForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required])
+  })
+
+  onSubmit(){
+    if(this.loginForm.valid){
+      const loginData = {
+        username: this.loginForm.get('username')?.value ?? '',
+        password: this.loginForm.get('password')?.value ?? ''
+      };
+
+      console.log(loginData);
+      this.authService.login(loginData)
+        .subscribe((data: any) => {
+          if(this.authService.isAuthenticated()){
+            this.router.navigate(['/user']);
+          }
+          console.log(data);
+        });
     }
   }
 }
